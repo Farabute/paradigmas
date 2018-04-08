dc573cac62dd436c26aec5073c360d067bae6737
Unknown <mariano.antico@gmail.com>
2018-04-08 10:20:53 -0300

    TP 1
    Cambio de nombre en variables
    Agregue todas las acciones faltantes

--- Full_coins_v01.hs
+++ Full_coins_v01.hs
@@ -11,20 +11,51 @@ data Persona = Persona {
 } deriving (Show, Eq)
 
 pepe = Persona "Jose" 10
+pepe2 = Persona "Jose" 20
 lucho = Persona "Luciano" 2
 
-nuevaBilletera unNumero unaBilletera = unaBilletera {billetera = unNumero}
+nuevaBilletera unNumero unaPersona = unaPersona {billetera = unNumero}
 
 -- Eventos --
 
-depositar plata usuario = nuevaBilletera (billetera usuario + plata) usuario
+depositar plata unaPersona = nuevaBilletera (billetera unaPersona + plata) unaPersona
 
-extraccion plata usuario | billetera usuario - plata < 0 = nuevaBilletera 0 usuario
-                         | otherwise = nuevaBilletera (billetera usuario - plata) usuario
+extraccion plata unaPersona | billetera unaPersona - plata < 0 = nuevaBilletera 0 unaPersona
+                            | otherwise = nuevaBilletera (billetera unaPersona - plata) unaPersona
 
-upgrade usuario | billetera usuario * 0.2 > 10 = nuevaBilletera (billetera usuario + 10) usuario
-                | otherwise = nuevaBilletera (billetera usuario * 1.2) usuario
+upgrade unaPersona | billetera unaPersona * 0.2 > 10 = nuevaBilletera (billetera unaPersona + 10) unaPersona
+                   | otherwise = nuevaBilletera (billetera unaPersona * 1.2) unaPersona
 
-cierreDeCuenta usuario = nuevaBilletera 0 usuario
+cierreDeCuenta unaPersona = nuevaBilletera 0 unaPersona
 
-quedaIgual usuario = nuevaBilletera usuario
+quedaIgual unaPersona = nuevaBilletera (billetera unaPersona) unaPersona
+
+-- Transacciones
+
+type Transaccion = Persona -> Persona
+luchoCierraLaCuenta :: Transaccion
+pepeDeposita5Monedas :: Transaccion
+
+luchoCierraLaCuenta unaPersona | nombre unaPersona == "Luciano" = cierreDeCuenta unaPersona
+                               | otherwise = quedaIgual unaPersona
+
+pepeDeposita5Monedas unaPersona | nombre unaPersona == "Jose" = depositar 5 unaPersona
+                                | otherwise = quedaIgual unaPersona
+
+-- Nuevos Eventos
+
+tocoYMeVoy = cierreDeCuenta.upgrade.(depositar 15)
+
+ahorranteErrante = (depositar 10).upgrade.(depositar 8).(extraccion 1).(depositar 2).(depositar 1)
+
+luchoTocaYSeVa unaPersona | nombre unaPersona == "Luciano" = tocoYMeVoy unaPersona
+                          | otherwise = quedaIgual unaPersona
+
+luchoEsUnAhorranteErrante unaPersona | nombre unaPersona == "Luciano" = ahorranteErrante unaPersona
+                                     | otherwise = quedaIgual unaPersona
+
+-- Pago entre usuarios
+
+pepeLeDa7UnidadesALucho unaPersona | nombre unaPersona == "Jose" = extraccion 7 unaPersona
+                                   | nombre unaPersona == "Luciano" = depositar 7 unaPersona
+                                   | otherwise = quedaIgual unaPersona
