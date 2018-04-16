2bc99428de2036867df0b2e057cdba1263889676
Unknown <jdufourg09@gmail.com>
2018-04-16 19:51:03 -0300

    Upgrade2 Full_coins_v01.hs
    Agregue, modifique y quite parte de los tests tomando en cuenta los enunciados.

--- Full_coins_v01.hs
+++ Full_coins_v01.hs
@@ -4,36 +4,48 @@ import Data.List
 import Data.Maybe
 import Test.Hspec
 
--- Test
-
-ejecutarTests = hspec $ do
-  describe "Probando los eventos\n" $ do
-    it "Pepe deposita 15 monedas" (depositar 15 pepe `shouldBe` nuevaBilletera 25 pepe)
-    it "Lucho extrae 5 monedas" (extraccion 5 lucho `shouldBe` nuevaBilletera 0 lucho)
-    it "Pepe recibe un upgrade" (upgrade pepe `shouldBe` nuevaBilletera 12 pepe)
-    it "Pepe cerro su cuenta" (cierreDeCuenta pepe `shouldBe` nuevaBilletera 0 pepe)
-    it "Lucho no cambio su billetera" (quedaIgual lucho `shouldBe` nuevaBilletera 2 lucho)
-    it "Pepe toca y se va" (tocoYMeVoy pepe `shouldBe` nuevaBilletera 0 pepe)
-    it "Pepe es un ahorrante errante" (ahorranteErrante pepe `shouldBe` nuevaBilletera 34 pepe)
-  describe "\nProbando las transacciones\n" $ do
-    it "Lucho cierra la cuenta" (luchoCierraLaCuenta lucho `shouldBe` nuevaBilletera 0 lucho)
-    it "Pepe deposita 5 monedas" (pepeDeposita5Monedas pepe `shouldBe` nuevaBilletera 15 pepe)
-    it "Lucho toca y se va" (luchoTocaYSeVa lucho `shouldBe` nuevaBilletera 0 lucho)
-    it "Lucho es un ahorrante errante" (luchoEsUnAhorranteErrante lucho `shouldBe` nuevaBilletera 24.400002 lucho)
-  describe "\nProbando el pago entre usuarios\n" $ do
-    it "Pepe le da 7 unidades a Lucho" (pepeLeDa7UnidadesALucho pepe `shouldBe` nuevaBilletera 3 pepe)
-    it "Pepe le da 7 unidades a Lucho" (pepeLeDa7UnidadesALucho lucho `shouldBe` nuevaBilletera 9 lucho)
-
 data Persona = Persona {
     nombre :: String,
     billetera :: Float
 } deriving (Show, Eq)
 
 pepe = Persona "Jose" 10
-pepe2 = Persona "Jose" 20
 lucho = Persona "Luciano" 2
+pepe2 = Persona "Jose" 20
+billeteraDe10Creditos = Persona "billeteraDe10Creditos" 10
 
+-- Test
+ejecutarTests = hspec $ do
+  describe "Probando los eventos\n" $ do
+  it "billeteraDe10Creditos sufre deposito" $ depositar 10 billeteraDe10Creditos `shouldBe` nuevaBilletera 20 billeteraDe10Creditos
+  it "billeteraDe10Creditos sufre extraccion de 3 unidades" $ extraccion 3 billeteraDe10Creditos `shouldBe` nuevaBilletera 7 billeteraDe10Creditos
+  it "billeteraDe10Creditos sufre extraccion de 15 unidades" $ extraccion 15 billeteraDe10Creditos `shouldBe` nuevaBilletera 0 billeteraDe10Creditos
+  it "billeteraDe10Creditos sufre un upgrade" $ upgrade billeteraDe10Creditos `shouldBe` nuevaBilletera 12 billeteraDe10Creditos
+  it "billeteraDe10Creditos sufre cierre de cuenta" $ cierreDeCuenta billeteraDe10Creditos `shouldBe` nuevaBilletera 0 billeteraDe10Creditos
+  it "billeteraDe10Creditos queda igual" $ quedaIgual billeteraDe10Creditos `shouldBe` nuevaBilletera 10 billeteraDe10Creditos
+  it "billeteraDe10Creditos sufre deposito de 1000 unidades y luego un upgrade" $  (upgrade.depositar 1000) billeteraDe10Creditos `shouldBe` nuevaBilletera 1020 billeteraDe10Creditos
+  describe "\nProbando los eventos en Usuarios\n" $ do
+  it "Estado de la billetera de pepe" $ billetera pepe `shouldBe` 10
+  it "Pepe cerro su cuenta" $ cierreDeCuenta pepe `shouldBe` nuevaBilletera 0 pepe
+  it "Pepe deposita 15 monedas, extrae 2 y obtiene un upgrade" $ (upgrade.extraccion 2.depositar 15) pepe `shouldBe` nuevaBilletera 27.6 pepe
+  describe "\nProbando las transacciones\n" $ do
+  it "Pepe realiza transacción 1" $ luchoCierraLaCuenta pepe `shouldBe` pepe
+  it "Pepe2 realiza transacción 1" $ luchoCierraLaCuenta pepe2 `shouldBe` pepe2
+  it "Pepe realiza transacción 2" $ pepeDeposita5Monedas pepe `shouldBe` nuevaBilletera 15 pepe
+  it "Pepe2 realiza transacción 2 con billetera de 50 unidades" $ (pepeDeposita5Monedas. depositar 30) pepe2 `shouldBe` nuevaBilletera 55 pepe2
+  it "Lucho toca y se va" $ luchoTocaYSeVa lucho `shouldBe` nuevaBilletera 0 lucho
+  it "Lucho es un ahorrante errante" $ luchoEsUnAhorranteErrante lucho `shouldBe` nuevaBilletera 24.400002 lucho
+  it "Pepe toca y se va" $ tocoYMeVoy pepe `shouldBe` nuevaBilletera 0 pepe
+  it "Pepe es un ahorrante errante" $ ahorranteErrante pepe `shouldBe` nuevaBilletera 34 pepe
+  describe "\nProbando el pago entre usuarios\n" $ do
+  it "Pepe le da 7 unidades a Lucho" $ pepeLeDa7UnidadesALucho pepe `shouldBe` nuevaBilletera 3 pepe
+  it "Pepe le da 7 unidades a Lucho" $ pepeLeDa7UnidadesALucho lucho `shouldBe` nuevaBilletera 9 lucho
+  
+  
+-- Operaciones con Personas --
+	
 nuevaBilletera unNumero unaPersona = unaPersona {billetera = unNumero}
+
 
 -- Eventos --
 
@@ -49,7 +61,7 @@ cierreDeCuenta unaPersona = nuevaBilletera 0 unaPersona
 
 quedaIgual unaPersona = nuevaBilletera (billetera unaPersona) unaPersona
 
--- Transacciones
+-- Transacciones --
 
 type Transaccion = Persona -> Persona
 luchoCierraLaCuenta :: Transaccion
@@ -61,7 +73,7 @@ luchoCierraLaCuenta unaPersona | nombre unaPersona == "Luciano" = cierreDeCuenta
 pepeDeposita5Monedas unaPersona | nombre unaPersona == "Jose" = depositar 5 unaPersona
                                 | otherwise = quedaIgual unaPersona
 
--- Nuevos Eventos
+-- Nuevos Eventos --
 
 tocoYMeVoy = cierreDeCuenta.upgrade.(depositar 15)
 
@@ -73,7 +85,7 @@ luchoTocaYSeVa unaPersona | nombre unaPersona == "Luciano" = tocoYMeVoy unaPerso
 luchoEsUnAhorranteErrante unaPersona | nombre unaPersona == "Luciano" = ahorranteErrante unaPersona
                                      | otherwise = quedaIgual unaPersona
 
--- Pago entre usuarios
+-- Pago entre usuarios --
 
 pepeLeDa7UnidadesALucho :: Transaccion
 pepeLeDa7UnidadesALucho unaPersona | nombre unaPersona == "Jose" = extraccion 7 unaPersona
