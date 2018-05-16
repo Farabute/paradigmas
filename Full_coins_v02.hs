04d01764d9a3f59f8f15e1dd26779ca4a21cb9ef
Unknown <mariano.antico@gmail.com>
2018-05-16 13:12:23 -0300

    Agregamos Bloques

--- Full_coins_v02.hs
+++ Full_coins_v02.hs
@@ -130,6 +130,22 @@ impactarTransaccion2 unaPersona unaTransaccion = nuevaBilletera (unaTransaccion 
 
 -- Bloque --
 
+-- Test bloques
+
+testBloques = hspec $ do
+  describe "\nProbando los bloques\n" $ do
+    it "21. Aplicamos bloque1 a Pepe y deberia resultar un nuevo Pepe con 18 unidades en su billetera" $ aplicarBloque bloque1 pepe `shouldBe` nuevaBilletera 18 pepe
+    it "22. Aplicamos bloque1 a los usuarios Pepe y Lucho, deberiamos chequear que Pepe es el unico con un saldo mayor a 10 en su billetera" $ usuariosConSaldoMayorANumero 10 bloque1 usuarios `shouldBe` [nuevaBilletera 10 pepe]
+    --it "23. Aplicamos bloque1 a los usuarios Pepe y Lucho, deberiamos chequear que Pepe es el mas adinerado" $ masAdinerado usuarios bloque1 `shouldBe` nuevaBilletera 18 pepe
+    --it "24. Aplicamos bloque1 a los usuarios Pepe y Lucho, deberiamos chequear que Lucho es el menos adinerado" $ menosAdinerado usuarios bloque1 `shouldBe` nuevaBilletera 0 lucho
+    --it "25. Aplicamos nuestro Block Chain a Pepe, deberiamos chequear que el bloque que lo deja con peor saldo es el bloque1, y lo deja con una billetera de 18" $ determinarPeorBloque pepe cadenaBloques `shouldBe` nuevoPeorBloque pepe [bloque1,bloque1,bloque1,bloque1]
+    it "26. Aplicamos nuestro Block Chain a Pepe, deberia quedar con una billetera de 115" $ aplicarBlockChainAUsuario blockChain pepe `shouldBe` nuevaBilletera 115 pepe
+    it "27. Aplicamos los primeras 3 bloques de nuestro Block Chain a Pepe, deberia quedar con una billetera de 51" $ saldoEnCiertoPuntoDeBlockChain 3 blockChain pepe `shouldBe` nuevaBilletera 51 pepe
+    it "28. Aplicamos nuestro Block Chain a Pepe y Lucho. Pepe deberia quedar con una billetera de 115 y Lucho con 0" $ (sum.(map billetera).aplicarBlockChainAConjuntoDeUsuarios blockChain) usuarios `shouldBe` 115
+    it "29. Deberían ser 11 bloques de nuestra Block Chain para que Pepe supere los 10000 en su billetera" $ cuantosBloquesNecesito pepe blockChainInfinita 1 `shouldBe` 11
+    -- Para resolver esto último se hace uso del concepto de evaluación diferida, la cual implica que al generar una lista infinita, se procesarán los elementos a medida que se generan.
+    -- En este caso, se tomaron elementos de una blockchain infinita hasta que se dio la condición de que el saldo sea mayor a 10000.
+
 type Bloque = [Transaccion]
 
 bloque1 :: Bloque
@@ -139,7 +155,10 @@ bloque2 :: Bloque
 bloque2 = [transaccion2,transaccion2,transaccion2,transaccion2,transaccion2]
 
 type Usuarios = [Persona]
-usuarios = [pepe, lucho]
+--piko = Persona "Mariano" 60
+--usuarios = [pepe,lucho, piko ,pepe ]
+usuarios = [pepe,lucho]
+
 
 transaccionAUsuario = map (impactarTransaccion)
 
@@ -148,3 +167,27 @@ aplicarBloque unBloque unaPersona= foldr ($) unaPersona (transaccionAUsuario unB
 
 usuariosConSaldoMayorANumero :: Billetera -> Bloque -> Usuarios -> Usuarios
 usuariosConSaldoMayorANumero unNumero unBloque = filter ((> unNumero).billetera.aplicarBloque unBloque)
+
+masAdinerado unosUsuarios unBloque = filter ((> ((billetera.head) unosUsuarios)).billetera.aplicarBloque unBloque) unosUsuarios
+
+type BlockChain = [Bloque]
+blockChain :: BlockChain
+blockChain = bloque2 : take 10 (repeat bloque1)
+
+aplicarBlockChainAUsuario :: BlockChain -> Persona -> Persona
+aplicarBlockChainAUsuario unaBlockChain = aplicarBloque (concat unaBlockChain)
+
+saldoEnCiertoPuntoDeBlockChain :: Int -> BlockChain -> Persona -> Persona
+saldoEnCiertoPuntoDeBlockChain unNumero unaBlockChain = aplicarBlockChainAUsuario (take unNumero unaBlockChain)
+
+aplicarBlockChainAConjuntoDeUsuarios :: BlockChain -> Usuarios -> Usuarios
+aplicarBlockChainAConjuntoDeUsuarios unaBlockChain = map (aplicarBloque (concat unaBlockChain))
+
+generarBlockChainInfinita :: Bloque -> BlockChain
+generarBlockChainInfinita unBloque = unBloque : generarBlockChainInfinita (unBloque ++ unBloque)
+
+blockChainInfinita :: BlockChain
+blockChainInfinita = generarBlockChainInfinita bloque1
+
+cuantosBloquesNecesito unaPersona unaBlockChain unNumero | billetera unaPersona < 10000 = cuantosBloquesNecesito (saldoEnCiertoPuntoDeBlockChain unNumero unaBlockChain unaPersona) unaBlockChain (unNumero + 1)
+                                                         | otherwise = unNumero
